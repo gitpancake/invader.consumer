d71d39b1e2226da58d06e3765a672428b587037a
henry <13052648+gitpancake@users.noreply.github.com>
2025-06-29 17:02:03 -0700

    Initial commit - consumer

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { ConsumeMessage } from "amqplib";
+import axios from "axios";
+import { config } from "dotenv";
+import { Flash } from "./types/Flash";
+import { RabbitMQBaseConsumer } from "./util/rabbitmq";
+
+config({
+  path: ".env",
+});
+
+const REGION = process.env.AWS_REGION;
+const BUCKET_NAME = process.env.BUCKET_NAME;
+const s3 = new S3Client({ region: REGION });
+const BASE_URL = "https://api.space-invaders.com";
+
+class MyConsumer extends RabbitMQBaseConsumer {
+  constructor() {
+    super("RABBITMQ_QUEUE");
+  }
+
+  protected async handleMessage(msg: ConsumeMessage): Promise<void> {
+    const content = msg.content.toString();
+    const flash: Flash = JSON.parse(content);
+    const imageUrl = BASE_URL + flash.img;
+    const s3Key = flash.img.replace(/^\//, "");
+    console.log(`[MyConsumer] Downloading image from: ${imageUrl}`);
+    try {
+      const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
+      const contentType = response.headers["content-type"] || "image/jpeg";
+      await s3.send(
+        new PutObjectCommand({
+          Bucket: BUCKET_NAME,
+          Key: s3Key,
+          Body: response.data,
+          ContentType: contentType,
+          ACL: "public-read",
+        })
+      );
+      console.log(`[MyConsumer] Uploaded image to S3: ${s3Key}`);
+    } catch (err) {
+      console.error("[MyConsumer] Error downloading/uploading image:", err);
+      throw err;
+    }
+  }
+}
+
+(async () => {
+  const consumer = new MyConsumer();
+  await consumer.startConsuming();
+})();
---
+++ src/util/rabbitmq/index.ts
@@ -0,0 +1,41 @@
+import { connect, ConsumeMessage } from "amqplib";
+
+/**
+ * Usage: Extend this class and implement handleMessage(msg, channel) using this.contractService
+ */
+export abstract class RabbitMQBaseConsumer {
+  protected rabbitUrl: string;
+  protected queue: string;
+
+  constructor(queueEnvVar: string) {
+    this.rabbitUrl = process.env.RABBITMQ_URL!;
+    this.queue = process.env.RABBITMQ_QUEUE!;
+
+    if (!this.rabbitUrl) throw new Error("RABBITMQ_URL is not defined in the environment variables");
+    if (!this.queue) throw new Error(`${queueEnvVar} is not defined in the environment variables`);
+  }
+
+  protected abstract handleMessage(msg: ConsumeMessage): Promise<void>;
+
+  public async startConsuming() {
+    const connection = await connect(this.rabbitUrl);
+    const channel = await connection.createChannel();
+    await channel.assertQueue(this.queue, { durable: true });
+    console.log(`[RabbitMQBaseConsumer] Waiting for messages in ${this.queue}. To exit press CTRL+C`);
+    channel.consume(
+      this.queue,
+      async (msg) => {
+        if (msg) {
+          try {
+            await this.handleMessage(msg);
+            channel.ack(msg);
+          } catch (err) {
+            console.error("[RabbitMQBaseConsumer] Error processing message:", err);
+            channel.nack(msg, false, false);
+          }
+        }
+      },
+      { noAck: false }
+    );
+  }
+}
